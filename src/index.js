1489d76475f0584ecde6e0f4a4c3925580538a01
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-11-30 09:12:58 -0700

    create reducer files

--- src/index.js
+++ src/index.js
@@ -4,3 +4,17 @@ import { creatStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+
+import App from "./App";
+
+const store = creatStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  document.getElementById('root')
+);
