dcd04ff1fd3550f6584df8a86cc4dcff3dc02c25
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-11-30 09:06:42 -0700

    install redux, thunk react-redux and dev tools, import to index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { creatStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
