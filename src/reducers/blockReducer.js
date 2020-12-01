f26da2668c768499f86190a6f31cb382eefc4c45
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-12-01 11:02:07 -0700

    update block reducer to have add_part

--- src/reducers/blockReducer.js
+++ src/reducers/blockReducer.js
@@ -2,6 +2,8 @@ export const blockReducer = (state = [], action) => {
   switch (action.type) {
     case "FETCH_BLOCKS":
       return action.payload;
+    case "ADD_PART":
+      return [...state, action.payload];
     default:
       return state;
   }
